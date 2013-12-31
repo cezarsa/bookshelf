d54abeff3da68483e70d289be97206438f3f1c88
Cezar Sa Espinola <cezar.sa@corp.globo.com>
2013-12-31 15:26:48 -0300

    books in 3d

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 
     var scrollElement = $('html, body'),
         booksArea = $('.books'),
-        items = booksArea.find('> div'),
+        items = booksArea.find('.book'),
         $window = $(window),
         scrollTimeout = null;
 
@@ -22,32 +22,34 @@
             acc = 0,
             lastBreak = 0;
 
-        booksArea.width(width).find('br').remove();
-        items.stop(true, false).css('margin-left', 0).removeClass('break');
+        booksArea.width(width);
+        items.stop(true, false);
+        booksArea.find('.book').appendTo(booksArea);
+        booksArea.find('.line').remove();
+
+        var line = $('<div class="line">'),
+            lineWidth = 0;
 
         for (var i = 0, len = items.length; i < len; ++i) {
             var item = items.eq(i),
                 pos = item.offset();
-            item.data('amount', 0);
-            if (i === len - 1) {
-                if (pos.left + item.width() - acc > width) {
-                    items.eq(lastBreak).data('amount', pos.left + item.width() - acc - width);
-                }
-            }
-            if ((pos.left - acc) > width) {
-                items.eq(lastBreak).data('amount', pos.left - acc - width);
-                acc = pos.left;
-                toBreak.push(i);
-                lastBreak = i;
+
+            lineWidth += item.width();
+            line.append(item);
+
+            if (lineWidth > width) {
+                line.data('amount', lineWidth - width);
+                booksArea.append(line);
+                lineWidth = 0;
+                line = $('<div class="line">');
             }
         }
 
-        for (i = 0; i < toBreak.length; ++i) {
-            items.eq(toBreak[i]).addClass('break');
-            items.eq(toBreak[i]).before('<br>');
+        if (lineWidth > 0) {
+            booksArea.append(line);
         }
 
-        items.eq(0).add('> div.break', booksArea).each(function() {
+        booksArea.find('.line').each(function() {
             var el = $(this),
                 duration = Math.max(1000, 40 * parseInt(el.data('amount'), 10));
 
@@ -114,5 +116,15 @@
             scrollElement.stop(true, false);
         }
     });
+
+    booksArea.on('mouseenter', '.book', function() {
+        var el = $(this);
+        el.addClass('three-d');
+    });
+    booksArea.on('mouseleave', '.book', function() {
+        var el = $(this);
+        el.removeClass('three-d');
+    });
+
     booksArea.imagesLoaded(update);
 }(jQuery));
